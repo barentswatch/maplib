ffb05939789fff10786913f88e805387a75e7375
Johan S <johan.sundin@norconsult.com>
2014-12-19 13:37:04 +0100

    Style stuff for OL3

---
+++ src/BW.MapImplementation/OL3/Styles/BW.MapImplementation.OL3.Styles.Measure.js
@@ -0,0 +1,28 @@
+var BW = BW || {};
+BW.MapImplementation = BW.MapImplementation || {};
+BW.MapImplementation.OL3 = BW.MapImplementation.OL3 || {};
+BW.MapImplementation.OL3.Styles = BW.MapImplementation.OL3.Styles || {};
+
+BW.MapImplementation.OL3.Styles.Measure = function(){
+    var styles = function(){
+        return new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: 'rgba(255, 255, 255, 0.8)'
+            }),
+            stroke: new ol.style.Stroke({
+                color: '#ffcc33',
+                width: 2
+            }),
+            image: new ol.style.Circle({
+                radius: 7,
+                fill: new ol.style.Fill({
+                    color: '#ffcc33'
+                })
+            })
+        });
+    };
+
+    return {
+        Styles: styles
+    };
+};
