5f689622e6e706017bed863a02fafdee14a2bb17
Atle Frenvik Sveen <atle.sveen@bouvet.no>
2015-01-23 12:20:57 +0100

    update paths for buster tests

--- test/buster.js
+++ test/buster.js
@@ -5,13 +5,13 @@ config['Njord Browser tests'] = {
     env: 'browser',
     rootPath: '../',
     libs: [
-        'lib/underscore-min.js',
-        'lib/backbone-min.js',
+        'lib/underscore/underscore-min.js',
+        'lib/backbone/backbone.js',
         'lib/OpenLayers-2.13.1/OpenLayers.js',
     ],
     sources: [
-        'src/mapcore.js',
-        'src/CircleDraw.js'
+        'src/ol2/Mapcore.js',
+        'src/ol2/CircleDraw.js'
     ],
     tests: [
         'test/*-test.js'
