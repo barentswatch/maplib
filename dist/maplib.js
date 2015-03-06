efe6ede4859ba18282a5d4aa80a9dbec5821f341
Amund Nedland <amund.nedland@norconsult.com>
2015-03-06 13:37:38 +0100

    Time support, improving code

--- dist/maplib.js
+++ dist/maplib.js
@@ -2765,6 +2765,7 @@ BW.MapImplementation.OL3.Time = function() {
     var arrayLength;
     var jsonDates = [];
 
+    // Retrieve the getCapabilities data from a given service url
     function getCapabilitiesJson(url) {
         url = url.replace(/\?$/g,'');   // remove trailing "?"
         var capabilitiesUrl = url + "?SERVICE=WMS&REQUEST=GetCapabilities&version=1.3.0";
@@ -2774,18 +2775,7 @@ BW.MapImplementation.OL3.Time = function() {
         });
     }
 
-    function getCurrent(dateArray) {
-        var current = '';
-        arrayLength = dateArray.length;
-        for (var j = 0; j < arrayLength; j++) {
-            if (moment(dateArray[j]) < moment()) {
-                current = timeArray[j];
-            }
-            else {
-                return current;
-            }
-        }
-    }
+    // Parse the capability info and get a json structure containing the dates, resolution and most current result
     function getWmsTime(capabilityResponse, layerName) {
         var parser = new ol.format.WMSCapabilities();
         var jsonCapabilities = parser.read(capabilityResponse);
@@ -2800,139 +2790,144 @@ BW.MapImplementation.OL3.Time = function() {
                     arrayLength = timeArray.length;
 
                     for (var j = 0; j < arrayLength; j++) {
-                        var jsonItem = analyzeDate(timeArray[j]);
+                        var jsonItem = _analyzeDate(timeArray[j]);
                         jsonDates.push(jsonItem);
                     }
-                    var resolution = getResolution();
+                    var resolution = _getResolution();
 
-                    var result = {
+                    return {
                         "dates": jsonDates,
                         "resolution": resolution,
-                        "current": getCurrent(timeArray)
+                        "current": _getCurrent(timeArray)
                     };
-
-                    return result;
                 }
             }
         }
         return undefined;
     }
 
+    /////////////////////////////
+    // Internal helper methods //
+    /////////////////////////////
 
-    function getResolution() {
+    // Get the time nearest before current time
+    function _getCurrent(dateArray) {
+        var current = '';
+        arrayLength = dateArray.length;
+        for (var j = 0; j < arrayLength; j++) {
+            if (moment(dateArray[j]) < moment()) {
+                current = dateArray[j];
+            }
+            else {
+                return current;
+            }
+        }
+    }
+
+    // helper method when determining/validating the data resolution
+    function _updateResolution(resolution, newResolution){
+        if (resolution === undefined) {
+            // First time called, set resolution
+            resolution = newResolution;
+        }
+        else if (resolution !== newResolution) {
+            // Different resolutions not supported
+            return "error";
+        }
+        return resolution;
+    }
+
+    // Find resolution of the time array
+    function _getResolution() {
         var resolution;
+        var newResolution;
 
         for (var k = 1; k < arrayLength; k++) {
-            var prev = json_dates[k - 1];
-            var curr = json_dates[k];
+
+            var prev = jsonDates[k - 1];
+            var curr = jsonDates[k];
             if (prev.type != curr.type) {
                 // Different time formats, not supported
                 return "error";
             }
 
-            dMinute = (curr.Minutes !== undefined) ? curr.Minutes - prev.Minutes : 0;
+            resolution = _updateResolution(resolution, newResolution);
+
+            var dMinute = (curr.Minutes !== undefined) ? curr.Minutes - prev.Minutes : 0;
             if (dMinute !== 0) {
-                if (resolution === undefined) {
-                    resolution = "Minutes";
-                }
-                else if (resolution !== "Minutes") {
-                    // Different resolutions not supported
-                    return "error";
-                }
+                newResolution = "Minutes";
                 continue;
             }
 
-            dHour = (curr.Hours !== undefined) ? curr.Hours - prev.Hours : 0;
+            var dHour = (curr.Hours !== undefined) ? curr.Hours - prev.Hours : 0;
             if (dHour !== 0) {
-                if (resolution === undefined) {
-                    resolution = "Hour";
-                }
-                else if (resolution !== "Hour") {
-                    // Different resolutions not supported
-                    return "error";
-                }
+                newResolution = "Hour";
                 continue;
             }
 
-            dDay = (curr.Day !== undefined) ? curr.Day - prev.Day : 0;
+            var dDay = (curr.Day !== undefined) ? curr.Day - prev.Day : 0;
             if (dDay !== 0) {
-                if (resolution === undefined) {
-                    resolution = "Day";
-                }
-                else if (resolution !== "Day") {
-                    // Different resolutions not supported
-                    return "error";
-                }
+                newResolution = "Day";
                 continue;
             }
 
-            dMonth = (curr.Month !== undefined) ? curr.Month - prev.Month : 0;
+            var dMonth = (curr.Month !== undefined) ? curr.Month - prev.Month : 0;
             if (dMonth !== 0) {
-                if (resolution === undefined) {
-                    resolution = "Month";
-                }
-                else if (resolution !== "Month") {
-                    // Different resolutions not supported
-                    return "error";
-                }
+                newResolution = "Month";
                 continue;
             }
 
-            dYear = curr.Year - prev.Year;
+            var dYear = curr.Year - prev.Year;
             if (dYear !== 0) {
-                if (resolution === undefined) {
-                    resolution = "Year";
-                }
-                else if (resolution !== "Year") {
-                    // Different resolutions not supported
-                    return "error";
-                }
+                newResolution = "Year";
             }
         }
-        return resolution;
+
+        return _updateResolution(resolution, newResolution);
     }
 
-    function analyzeDate(datestring) {
+    // Validate date format and create date object
+    function _analyzeDate(datestring) {
         var type = "";
-        var dateformat = 0;
+        var dateFormat = 0;
         if (moment(datestring, "YYYY-MM-DDTHH:mm:ssZ").isValid()) {
-            dateformat = 1;
+            dateFormat = 1;
         }
         else if (moment(datestring, "YYYY-MM-DDTHH:mm").isValid()) {
-            dateformat = 2;
+            dateFormat = 2;
         }
         else if (moment(datestring, "YYYY-MM-DDTHH:mm:ss").isValid()) {
-            dateformat = 3;
+            dateFormat = 3;
         }
         else if (moment(datestring, "YYYY-MM-DDTHH:mm:ss").isValid()) {
-            dateformat = 4;
+            dateFormat = 4;
         }
         else if (moment(datestring, "YYYY-MM-DD HH:mm:ss").isValid()) {
-            dateformat = 5;
+            dateFormat = 5;
         }
         else if (moment(datestring, "YYYY-MM-DDTHH:mm").isValid()) {
-            dateformat = 6;
+            dateFormat = 6;
         }
         else if (moment(datestring, "YYYY-MM-DDTHH").isValid()) {
-            dateformat = 7;
+            dateFormat = 7;
         }
         else if (moment(datestring, "YYYY-MM-DD").isValid()) {
-            dateformat = 8;
+            dateFormat = 8;
         }
         else if (moment(datestring, "YYYY-MM").isValid()) {
-            dateformat = 9;
+            dateFormat = 9;
         }
         else if (moment(datestring, "YYYY").isValid()) {
-            dateformat = 10;
+            dateFormat = 10;
         }
         else if (moment(datestring, "THH:mm:ssZ").isValid()) {
-            dateformat = 11;
+            dateFormat = 11;
         }
         else if (moment(datestring, "THH:mm:ss").isValid()) {
-            dateformat = 12;
+            dateFormat = 12;
         }
         else if (moment(datestring, "YYYYMMDD").isValid()) {
-            dateformat = 13;
+            dateFormat = 13;
         }
         else { // invalid format
             return {};
@@ -2940,27 +2935,27 @@ BW.MapImplementation.OL3.Time = function() {
 
         var year, month, day, hour, minutes;
 
-        if (dateformat in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13]) {
+        if (dateFormat in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13]) {
             year = moment(datestring).year();
             type = "year";
         }
-        if (dateformat in [1, 2, 3, 4, 5, 6, 7, 8, 9, 13]) {
+        if (dateFormat in [1, 2, 3, 4, 5, 6, 7, 8, 9, 13]) {
             month = moment(datestring).month();
             type = "month";
         }
-        if (dateformat in [1, 2, 3, 4, 5, 6, 7, 8]) {
+        if (dateFormat in [1, 2, 3, 4, 5, 6, 7, 8]) {
             day = moment(datestring).day();
             type = "day";
         }
-        if (dateformat in [1, 2, 3, 4, 5, 6, 7, 11, 12]) {
+        if (dateFormat in [1, 2, 3, 4, 5, 6, 7, 11, 12]) {
             hour = moment(datestring).hours();
             type = "hour";
         }
-        if (dateformat in [1, 2, 3, 4, 5, 6, 7, 8]) {
+        if (dateFormat in [1, 2, 3, 4, 5, 6, 7, 8]) {
             minutes = moment(datestring).minutes();
             type = "minutes";
         }
-        var date_item = {
+        return {
             "Value": datestring,
             "Year": year,
             "Month": month,
@@ -2968,9 +2963,8 @@ BW.MapImplementation.OL3.Time = function() {
             "Hours": hour,
             "Minutes": minutes,
             "Type": type,
-            "Format": dateformat
+            "Format": dateFormat
         };
-        return date_item;
     }
 
     return {
