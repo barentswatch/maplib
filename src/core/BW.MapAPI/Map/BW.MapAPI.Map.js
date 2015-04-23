00a203f473261ef45be0d67dc3e5453a0467d77b
Amund Nedland <amund.nedland@norconsult.com>
2015-04-23 16:09:05 +0200

    start/stop waiting to load featureinfo - bugfix

--- src/core/BW.MapAPI/Map/BW.MapAPI.Map.js
+++ src/core/BW.MapAPI/Map/BW.MapAPI.Map.js
@@ -150,10 +150,10 @@ BW.MapAPI.Map = function(mapImplementation, eventHandler, featureInfo, layerHand
     }
 
     function startWaiting(){
-        featureInfo.StartWaiting();
+        mapImplementation.StartWaiting();
     }
     function stopWaiting(){
-        featureInfo.StopWaiting();
+        mapImplementation.StopWaiting();
     }
 
     function showHighlightedFeatures(features){
